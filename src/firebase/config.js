622f689998be0757eb8fafa16835d51a2932119e
Muhammad46Shams <shams,muhammad46@gmail.com>
2025-10-23 01:12:32 +0100

    make new firebase project and app and install firebase and config in our project

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNx-1nf3AieAgRK9BQa62Is2T29uxiL_A",
+  authDomain: "mymoney-50ffc.firebaseapp.com",
+  projectId: "mymoney-50ffc",
+  storageBucket: "mymoney-50ffc.firebasestorage.app",
+  messagingSenderId: "822821705279",
+  appId: "1:822821705279:web:c4e9511514cdc0776b0a8a"
+};
+
+// init services
+const projetFirestore = initializeApp(firebaseConfig);
+
+export { projetFirestore }
