cb25f72aec1d6e9c9145c4d1b3f7a18d3bdfbc40
Muhammad46Shams <shams,muhammad46@gmail.com>
2025-10-23 17:39:53 +0100

    implement auth context and use auth context js and save info and save user log

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,47 @@
 import { useState } from "react"
+import { useAuthContext } from "./useAuthContext";
 import { projectAuth } from "../firebase/config"
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 
 
 export const useSignup = () => {
 
     const [error, setError] = useState(null);
-    const [isPending, setIsPending] = useState(false); 
+    const [isPending, setIsPending] = useState(false);
+    
+    const { dispatch } = useAuthContext()
+    const signup = async (email,password, displayName) => {
+        setError(null)
+        setIsPending(true)
+        
+        try {
+            // signup user
+            const res =  await createUserWithEmailAndPassword(projectAuth,email,password)
+            console.log(res.user);
+
+            if(!res)
+            {
+                throw new Error('Could not complete the signup')
+            }
+            // add display name to user
+            await updateProfile(projectAuth.currentUser ,  { 
+                displayName: displayName 
+            })
+
+            // dispatch login action
+            dispatch({type: 'LOGIN', payload: res.user})
+
+            setError(null)
+            setIsPending(false)
+            
+        } catch (err) {
+            console.log(err);
+            console.log(err.message)
+            setError(err.message)
+            setIsPending(false)
+        }
+
+    }
+
+    return { signup, error, isPending }
 }
