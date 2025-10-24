ca7172b55fc3385ba55f2c2be93bc123c5010b78
Muhammad46Shams <shams,muhammad46@gmail.com>
2025-10-24 02:10:40 +0100

    wait until auth is ready

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import Home from './pages/Home/Home';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import Navbar from './components/Navbar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const {authIsReady } = useAuthContext()
   return (
     <div className="App">
-      <BrowserRouter>
-      {/* Navigation */}
-      <Navbar/>
+      {authIsReady && (
+        <BrowserRouter>
+          {/* Navigation */}
+          <Navbar/>
 
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+          {/* Routes */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
